a25385c411bfc760fe46af1fb74a4492fb394540
SandeepaDevin <48104360+SandeepaDevin@users.noreply.github.com>
2020-08-28 23:38:09 +0530

    Update stream.cpp

--- stream.cpp
+++ stream.cpp
@@ -1,8 +1,29 @@
-  char ch;
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    stringstream ss(str); vector <int> inte;
+    char ch;
     int temp;
     
-    while(ss)   // While the stringstream object does not hit a null byte
+    while(ss) 
     {
-        ss>>temp>>ch;   // Extract the comma seperated ints with the extraction >> operator
-        vec.push_back(temp);   // Push the int onto the vector
+        ss>>temp>>ch; 
+        inte.push_back(temp);
     } 
+    return inte;
+	
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
